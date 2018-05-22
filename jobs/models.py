51eebaad557583c932f32a8fe7391ecdc22537aa
Daniel Alviso <dan.alviso@yahoo.com>
2018-05-22 11:47:35 -0700

    Added changes to finish the project

--- jobs/models.py
+++ jobs/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Job(models.Model):
   image = models.ImageField(upload_to='images/')
   summary = models.CharField(max_length=200)
+  link = models.URLField(max_length=200, null=True)
